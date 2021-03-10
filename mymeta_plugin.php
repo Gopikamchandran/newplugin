30bdf77a5c69b7b5230e2597f1706c878d77aeea
Gopika Chandran <gopika82174@gmail.com>
2021-03-10 12:40:37 +0530

    update

--- mymeta_plugin.php
+++ mymeta_plugin.php
@@ -1,3 +1,4 @@
+
 <?php
 /**
  * Plugin Name: My Meta Plugin
@@ -9,12 +10,21 @@
  *License: GPLv2 or later
  *Text Domain: mymeta_plugin
  */
+?><!DOCTYPE html>
+<html><head>
+	<link href="style.css" rel="stylesheet">
+</head><body>
+<?php
+defined( 'ABSPATH' ) or die();
+class mymeta_plugin{
 
-
-add_action("admin_init","custom_metabox");
+function __construct(){
+add_action("admin_init",array($this,"custom_metabox"));
+add_action("save_post",array($this,"save_detail"));
+add_filter("the_content",array($this,"callback_fun"));
+}
 function custom_metabox(){
-	add_meta_box("custom_metabox_01","Custom Metabox","custom_metabox_field","post","normal","low");
-
+	add_meta_box("custom_metabox_01","Custom Metabox",array($this,"custom_metabox_field"),"post","normal","low");
 }
 function custom_metabox_field(){
 	global $post;
@@ -22,25 +32,40 @@ function custom_metabox_field(){
 	$data = get_post_custom($post->ID);
 	$val = isset($data['custom_input'])? esc_attr($data['custom_input'][0]): 'no value';
 
-	echo'<input type="text" name="custom_input" id="custom_input" value="'.$val.'"/>';
-	
-	$custom_checkbox=get_post_meta($post->ID,'custom_input_check',true);
-	if($custom_checkbox == 'yes')
-		$custom_checkbox_val='checked="checked"';
-		echo'<input type="checkbox" name="custom_input_check" id="custom_input_check" value="'.$val.'"/>';
-		echo'<label>Show</label>';
-}		
-function callback_fun()
-{
-	global $post;
-/*	
-	$custom_checkbox=get_post_meta($post->ID,'custom_input_check',true);
-	if($custom_checkbox == 'yes')
-		$custom_checkbox_val='checked="checked"';*/
-	$data = get_post_custom($post->ID);
-	$val = isset($data['custom_input_check'])? esc_attr($data['custom_input_check'][0]): 'no value';
+	echo'<input type="text" name="custom_input" id="custom_input" value="'.$val.'"/>';?>
+	<label for="custom_input_check">Show</label>
 
-	echo'<input type="text" name="custom_input" id="custom_input" value="'.$val.'"/>';
+	<?php
+	$custom_checkbox=esc_attr(get_post_meta($post->ID,'custom_input_check',true));
+	if($custom_checkbox == ""){
+		?>
+		<input name="custom_input_check" type="checkbox" value="true"/>
+		<?php
+	}
+	else if($custom_checkbox == "true"){
+		?>
+		<input name="custom_input_check" type="checkbox" value="true" checked/>
+		<?php
+	}
+
+		//$custom_checkbox_val='checked="checked"';
+	//	echo'<input type="checkbox" name="custom_input_check" id="custom_input_check" value="'.$val.'"/>';
+		//echo'<label>Show</label>';
+}		
+function callback_fun($content){
+	global $post;
+
+
+	$custom_checkbox = esc_attr(get_post_meta($post->ID,'custom_input_check',true));
+	if($custom_checkbox == "true"){
+		$callback_fun = esc_attr(get_post_meta($post->ID,'custom_input',true));
+		$post1 = "<div class='display_meta'>$callback_fun</div>";
+		return $post1 . $content;
+	}
+	else{
+		return $content;
+	}
+	
 
 }
 
@@ -49,15 +74,25 @@ function save_detail(){
 	if(defined('DOING_AUTOSAVE')&& DOING_AUTOSAVE){
 		return $post->ID;
 	}
-	//$chk = isset( $_POST['custom_input_check'] )  ? 'on' : 'off';
-    //update_post_meta( $post->ID, 'custom_input_check', $chk );
-	update_post_meta($post->ID,"custom_input",$_POST["custom_input"]);
-	update_post_meta($post->ID,"custom_input_check",$_POST["custom_input_check"]);
-	//echo $_POST["custom_input_check"];
+	
+	$txtbox = sanitize_text_field($_POST['custom_input']);
+	update_post_meta($post->ID,'custom_input',$txtbox);
+	$chkbox = sanitize_key($_POST['custom_input_check']);
+	update_post_meta($post->ID,'custom_input_check',$chkbox);
+	
 }
 
-add_action("save_post","save_detail");
-add_filter('the_content','callback_fun');
-
-
-?>
+}
+$mymeta = new mymeta_plugin();
+if (!function_exists('write_log')) {
+	function write_log ( $log )  {
+		if ( true === WP_DEBUG ) {
+			if ( is_array( $log ) || is_object( $log ) ) {
+				error_log( print_r( $log, true ) );
+			} else {
+				error_log( $log );
+			}
+		}
+	}
+} ?>
+</body></html>
